483da8d1736c5632f834df0e161daf7070dc997c
7cherish <wotjs08@hanmail.net>
2019-01-05 03:27:41 +0900

    전달확인프린트문수정, 아이디 중복검사 object Object뜨던거 수정, 아이디 정규식표현 수정, 회원가입폼에서 비밀번호 제외한 파라미터값 못가져가고 있음..

--- EscapeRoom/src/semi/member/controller/MemberDeleteServlet.java
+++ EscapeRoom/src/semi/member/controller/MemberDeleteServlet.java
@@ -8,20 +8,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import semi.member.model.service.MemberService;
-import semi.member.model.vo.Member;
 
 /**
- * Servlet implementation class CheckIdDuplicateServlet
+ * Servlet implementation class MemberDeleteServlet
  */
-@WebServlet("/member/checkIdDuplicate")
-public class CheckIdDuplicateServlet extends HttpServlet {
+@WebServlet("/member/memberDelete")
+public class MemberDeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public CheckIdDuplicateServlet() {
+    public MemberDeleteServlet() {
         super();
+        // TODO Auto-generated constructor stub
     }
 
 	/**
@@ -29,23 +29,25 @@ public class CheckIdDuplicateServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 1. 파라미터 핸들링
-		String userId = request.getParameter("userId");
-		System.out.println("userId@CheckIdDuplicateServlet = "+userId);
+		String userId = (String)request.getParameter("userId");
+		System.out.println("userId@MemberDeleteServlet = " + userId);
 		
-		// 2. 비즈니스로직처리
-		Member m = new MemberService().selectOne(userId);
+		int result = new MemberService().deleteMember(userId);
 		
-		// 사용가능여부를 isUsable변수에 담기
-		boolean isUsable = m == null?true:false;
+		String msg = "";
+		String loc = "/member/logout";
+		String view = "/WEB-INF/views/common/msg.jsp";
 		
-		// view단 jsp에서 사용할 데이터를 request의 속성값으로 보관
-		request.setAttribute("isUsable", isUsable);
-		request.setAttribute("userId", userId);
+		if(result>0){
+			msg = "회원탈퇴 완료!";
+		}
+		else{
+			msg = "회원탈퇴 실패!";
+		}
+		request.setAttribute("msg", msg);
+		request.setAttribute("loc", loc);
 		
-		// 3. view단 처리
-		// checkIdDuplicate.jsp
-		request.getRequestDispatcher("/WEB-INF/views/member/checkIdDuplicate.jsp")
-			   .forward(request, response);
+		request.getRequestDispatcher(view).forward(request,  response);
 	}
 
 	/**
