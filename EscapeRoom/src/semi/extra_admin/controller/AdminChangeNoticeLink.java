9d324acac7d968b8c30b87308dc50fae141094e3
rkdsun029 <rkdsun029@gmail.com>
2019-01-11 19:25:08 +0900

    일부 디렉토리 변경 및 기능 보완

--- EscapeRoom/src/semi/extra_admin/controller/AdminChangeNoticeLink.java
+++ EscapeRoom/src/semi/extra_admin/controller/AdminChangeNoticeLink.java
@@ -1,6 +1,7 @@
-package semi.admin.controller;
+package semi.extra_admin.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,20 +9,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import semi.home.model.service.HomeService;
 import semi.notice.model.service.noticeService;
 import semi.notice.model.vo.Notice;
 
 /**
- * Servlet implementation class AdminChangeNoticeLinkEnd
+ * Servlet implementation class AdminChangeNoticeLink
  */
-@WebServlet("/admin/changeNoticeLinkEnd")
-public class AdminChangeNoticeLinkEnd extends HttpServlet {
+@WebServlet("/admin/changeNoticeLink")
+public class AdminChangeNoticeLink extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public AdminChangeNoticeLinkEnd() {
+    public AdminChangeNoticeLink() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -31,14 +33,15 @@ public class AdminChangeNoticeLinkEnd extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		//현재 링크되어있는 공지사항 객체  및 공지사항 전체 리스트 호출
 		int noticeNo = Integer.parseInt(request.getParameter("noticeNo"));
 		Notice n = new noticeService().selectOneNotice(noticeNo);
 		
-		System.out.println("확인1 : " + noticeNo);
-		System.out.println("확인2 : " + n);
+		List<Notice> list = new HomeService().selectAllNotice();		
+		String view = "/WEB-INF/views/admin/changeNoticeLink.jsp";
 		
-		String view = "/home";
-		request.setAttribute("linkedNotice", n);
+		request.setAttribute("notice", n);
+		request.setAttribute("list", list);
 		request.getRequestDispatcher(view).forward(request, response);
 	}
 
