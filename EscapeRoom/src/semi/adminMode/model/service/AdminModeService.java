9828e35f06014f62cd71ca2448ef812b79b9a79c
rkdsun029 <rkdsun029@gmail.com>
2019-01-14 08:35:13 +0900

    관리자 신고접수

--- EscapeRoom/src/semi/adminMode/model/service/AdminModeService.java
+++ EscapeRoom/src/semi/adminMode/model/service/AdminModeService.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.util.List;
 
 import semi.adminMode.model.dao.AdminModeDao;
+import semi.board.solve.model.vo.SolveBoard;
 import semi.member.model.vo.Member;
 
 public class AdminModeService {
@@ -21,6 +22,17 @@ public class AdminModeService {
 		int result = 0;
 		Connection conn = getConnection();
 		result = new AdminModeDao().deleteProfile(conn, userId);
+		
+		if(result > 0) commit(conn);
+		else rollback(conn);
+		
+		return result;
+	}
+
+	public int insertSolvePostReport(SolveBoard sb, String reason, String userComment) {
+		int result = 0;
+		Connection conn = getConnection();
+		result = new AdminModeDao().insertSolvePostReport(conn, sb, reason, userComment);
 		
 		if(result > 0) commit(conn);
 		else rollback(conn);
--- EscapeRoom/src/semi/board/solve/controller/PostReportEnd.java
+++ EscapeRoom/src/semi/board/solve/controller/PostReportEnd.java
@@ -1,13 +1,16 @@
 package semi.board.solve.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import semi.adminMode.model.service.AdminModeService;
 import semi.board.solve.model.dao.SolveBoardDao;
+import semi.board.solve.model.vo.SolveBoard;
 
 /**
  * Servlet implementation class PostReportEnd
@@ -30,26 +33,34 @@ public class PostReportEnd extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int postNo = Integer.parseInt(request.getParameter("postNo"));
 		
+		String reason = request.getParameter("reason");
+		System.out.println(reason);
+		
+		String userComment = request.getParameter("userComment");
+		
 		int result = new SolveBoardDao().reportPost(postNo);		
 		
 		String view = "/WEB-INF/views/common/msg.jsp";
 		String msg = "";
 		String loc = "";
 		
+		SolveBoard sb = new SolveBoardDao().selectByNo(postNo);
+		
+		int insertReport = new AdminModeService().insertSolvePostReport(sb, reason, userComment);
 
-
-        if(result > 0) {
+        if(result > 0 && insertReport > 0) {
             msg = "신고접수를 완료하였습니다.";
             // 팝업창을 닫기 위한 코드
             String script = "self.close();";
             // jsp에 전달하기 위해 속성으로 전달
             request.setAttribute("script", script);
+            
          }else {
         	 msg="신고실패";
          }
         
 		request.setAttribute("msg", msg);
-		  request.setAttribute("loc", loc);
+		request.setAttribute("loc", loc);
 		request.getRequestDispatcher(view).forward(request, response);
 
 	}
